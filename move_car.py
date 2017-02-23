7c2bb5741344c115cb5fdac1c58acbfa7b081d64
Chanin Tolson <ctolson@email.unc.edu>
2017-02-23 17:56:08 -0500

    add comments

--- move_car.py
+++ move_car.py
@@ -1,13 +1,35 @@
-directions = ['N','E','S','W'] 
+# General:
+# 1) add comments
+# 2) add unit tests instead of printing tests
+# 3) state what python version is being used
+# 4) include author names
+
+# Line Item:
+# Missing comments line 15
+# Missing function line 21
+# Missing input tests line 30
+# Inefficient eval line 57
+# Unused code line 65
+# Repeated code line 70
+# Syntax problem line 79
+
+# Missing comments
+# describe what each of these parameters is encoding
+directions = ['N','E','S','W']
 movement = {'N': (0,1), 'E': (1,0), 'S': (0,-1), 'W':(-1,0)}
 commands = {'L': 'turn_left', 'R': 'turn_right', 'M': 'move'}
 
+# Missing function
+# What is the raw_input() function? Should it be called from another class or included here?
+# If it is from an older version of python, specify what version or allow for both
 GRID_MAX_X, GRID_MAX_Y = map(int, raw_input().split())
 
 first_vehicle_x = None
 first_vehicle_y = None
 
 class Vehicle():
+    # Missing input tests
+    # check to see if input is the correct type (or convert it to the correct type)
     def __init__(self, x, y, face):
         self.x = x
         self.y = y
@@ -29,17 +51,24 @@ class Vehicle():
             if new_y in xrange(GRID_MAX_Y+1):
                 self.y = new_y
 
+
 vehicle_one_pos = raw_input().split()
 vehicle_one_commands = raw_input()
 
+# Inefficient eval
+# remove the eval and replace with a faster, easier to debug solution
+# consider creating an empty array and filling
 vehicle_one = Vehicle(int(vehicle_one_pos[0]), int(vehicle_one_pos[1]), vehicle_one_pos[2])
 for command in vehicle_one_commands:
     eval("vehicle_one.{0}()".format(commands[command]))
 
+# Unused code
+# Remove these lines, if unsued
 first_vehicle_x = vehicle_one.x
 first_vehicle_y = vehicle_one.y
 
-
+# Repeated code
+# create a function that runs lines 55-63 and use that for both vehicle 1 and 2
 vehicle_two_pos = raw_input().split()
 vehicle_two_commands = raw_input()
 
@@ -47,5 +76,13 @@ vehicle_two = Vehicle(int(vehicle_two_pos[0]), int(vehicle_two_pos[1]), vehicle_
 for command in vehicle_two_commands:
     eval("vehicle_two.{0}()".format(commands[command]))
 
+# Syntax problem
+# print statement must have parentheses in python v3 and higher
 print vehicle_one.x, vehicle_one.y, vehicle_one.dir
 print vehicle_two.x, vehicle_two.y, vehicle_two.dir
+
+
+
+
+
+
